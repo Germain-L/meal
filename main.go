9ea2affbd2879c2518d3b2c2c20b3c631e36da75
Germain-L <germain.leignel@gmail.com>
2024-11-05 11:27:40 +0100

    Implement graceful server shutdown and logging enhancements

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
+	"log"
 	"meals/handlers"
 	"meals/middleware"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	log.Default().SetOutput(os.Stdout)
+
 	// Create a new ServeMux
 	mux := http.NewServeMux()
 
@@ -18,6 +26,34 @@ func main() {
 	// Wrap the mux with the middleware
 	handler := middleware.LoggingMiddleware(mux)
 
-	// Start the server
-	http.ListenAndServe(":8080", handler)
+	// Create an http.Server
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+
+	// Run the server in a goroutine
+	go func() {
+		log.Printf("Listening on port %s", "8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Create a context with timeout for the shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown the server
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown: %v", err)
+	}
+
+	log.Println("Server exited properly")
 }
