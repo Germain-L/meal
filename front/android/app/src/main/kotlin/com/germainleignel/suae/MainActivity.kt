662e1934977842d62ad374970c20683435765bcf
Germain-L <germain.leignel@gmail.com>
2024-11-09 11:19:00 +0100

    Remove unused files and configurations; add necessary Flutter and Android setup files

---
+++ front/android/app/src/main/kotlin/com/germainleignel/suae/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.germainleignel.suae
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- front/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import { Configuration, AuthenticationApi, SystemApi, UserApi } from '$lib/api-client';
-
-const config = new Configuration({
-	basePath: import.meta.env.VITE_API_BASE_PATH
-});
-
-export const authApi = new AuthenticationApi(config);
-export const systemApi = new SystemApi(config);
-export const userApi = new UserApi(config);
